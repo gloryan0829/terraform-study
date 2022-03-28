e2bc9b63ddb668385cf11f5a33b693f7a135273f
SamLee <candoublej@gmail.com>
2022-03-28 16:43:59 +0900

    첫 단독 서버 배포하기

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "first_example" {
-  ami           = "ami-033a6a056910d1137"
-  instance_type = "t2.micro"
-  subnet_id     = "	subnet-0933e21aa3354cdb8"
+  ami                         = "ami-033a6a056910d1137"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.instance.id]
+  subnet_id                   = "subnet-0933e21aa3354cdb8"
+  associate_public_ip_address = "true"
+  user_data                   = <<-EOF
+                #!/bin/bash
+                echo "Hello JJ" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
     "Name" = "terraform-control-node"
   }
